c03cedcb34c4170e5dd9e5c13e965728660d77d1
Shivam Kumar <kshivam0508@gmail.com>
2025-07-24 20:27:49 +0530

    24-07-2025 2

---
+++ Stack/Easy/q1047.java
@@ -0,0 +1,24 @@
+class q1047{
+    class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(int i=1;i<s.length();i++){
+            char c=s.charAt(i);
+            if( !stack.isEmpty()&& stack.peek()==c){
+            stack.pop();
+            }
+            
+            else{
+                stack.push(c);
+        }
+
+        }
+        StringBuilder sb=new StringBuilder();
+        for(char c:stack){
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
+}
