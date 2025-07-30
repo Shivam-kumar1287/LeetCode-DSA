324c34573c18e3bb6d599e873dac74a3f8f0f84f
Shivam Kumar <kshivam0508@gmail.com>
2025-07-30 23:07:35 +0530

    30-07-2025 add hard q25

---
+++ LinkedList/hard/q25.java
@@ -0,0 +1,44 @@
+package LinkedList.hard;
+
+public class q25 {public ListNode reverseKGroup(ListNode head, int k) {
+        int count = 0;
+        ListNode t = head;
+        while (t != null) {
+            count++;
+            t = t.next;
+        }
+
+        if (count < k) return head;
+
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> resultList = new ArrayList<>();
+        ListNode temp = head;
+        int groupCount = 0;
+
+        while (temp != null) {
+            l1.add(temp.val);
+            groupCount++;
+
+            if (groupCount == k) {
+                Collections.reverse(l1);     
+                resultList.addAll(l1);       
+                l1.clear();                 
+                groupCount = 0;
+            }
+
+            temp = temp.next;
+        }
+
+        resultList.addAll(l1);
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        for (int val : resultList) {
+            curr.next = new ListNode(val);
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
