2a171fe71c396a1e61864036ac973274993dea45
Shivam Kumar <kshivam0508@gmail.com>
2025-08-17 15:58:28 +0530

    LinkedList/Medium/q19.java

---
+++ LinkedList/Medium/q19.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class q19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+     int c=0;
+     ListNode temp=head;
+
+     while(temp!=null){
+        c++;
+        temp=temp.next;
+     }
+     int index=c-n+1;
+     if(index==1){
+        return head.next;
+     }
+    int  c1=1;
+    ListNode t=head;
+    while(t!=null){
+        if(c1==index-1){
+        t.next=t.next.next;
+        break;
+     }
+        t=t.next;
+        c1++;
+     
+    }
+       return head;    
+    }
+}
