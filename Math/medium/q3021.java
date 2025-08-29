c486c0034ab3f0a7a1223eadc740509ee1a6e017
Shivam Kumar <kshivam0508@gmail.com>
2025-08-29 13:33:59 +0530

    Math/medium/

---
+++ Math/medium/q3021.java
@@ -0,0 +1,29 @@
+package leetcode.Math.medium;
+
+class Solution {
+    public long flowerGame(int n, int m) {
+     long  co=0;
+    long   ce1=0;
+    long   co1=0;
+     long    ce=0;        
+        if (n % 2 == 0){
+            ce = n / 2;
+            co = n / 2;
+            }
+        else{
+            ce = n / 2;
+            co = n / 2 + 1;
+        }
+            
+        if( m % 2 == 0){
+            ce1 = m / 2;
+            co1 = m / 2;
+        }
+        else{
+            ce1 = m /2;
+            co1 = m / 2 + 1;
+        }
+            
+        return ce1 * co + co1 * ce;  
+    }
+}
