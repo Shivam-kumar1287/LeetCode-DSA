099662a3f45b3e7e3008db8d1f9545ff2b7e65f3
Shivam Kumar <kshivam0508@gmail.com>
2025-09-08 09:58:35 +0530

    }

---
+++ Math/Easy/q1317.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        for(int i=1;i<n;i++){
+            int a=i;
+            int b=n-i;
+            if(nonzero(a) && nonzero(b)){
+               return  new int[]{a,b};
+            }
+        }
+    return new int[]{};
+    }
+    private boolean nonzero(int n){
+        while(n>0){
+            if(n%10==0){
+                return false;
+            }
+            n/=10;
+        }
+        return true;
+    }
+}
