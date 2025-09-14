f378befc8284eab453ec87fa5bc94300fb17e77a
Shivam Kumar <kshivam0508@gmail.com>
2025-09-14 16:47:07 +0530

    Array/Easy/Easy/q268.java

--- Array/Easy/Easy/q268.java
+++ Array/Easy/Easy/q268.java
@@ -1,6 +1,6 @@
 class q268 {
     public int missingNumber(int[] nums) {
-        int n=nums.length;
+           int n=nums.length;
         int exp=(n*(n+1))/2;
         int sum=0;
            for(int i:nums){
