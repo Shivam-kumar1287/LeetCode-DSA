1b13ba1d010c52996fe976791c81129ba3f084f7
Shivam Kumar <kshivam0508@gmail.com>
2025-09-17 10:28:52 +0530

    Array/Easy/hard/q42.java

---
+++ Array/Easy/hard/q42.java
@@ -0,0 +1,24 @@
+class q42 {
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) return 0;
+        
+        int[] l = new int[n];
+        l[0] = height[0];  
+        for(int i = 1; i < n; i++) {
+            l[i] = Math.max(height[i], l[i-1]);
+        }
+        
+        int[] r = new int[n];
+        r[n-1] = height[n-1];
+        for(int i = n-2; i >= 0; i--) {
+            r[i] = Math.max(height[i], r[i+1]);
+        }
+        
+        int Water = 0;
+        for(int i = 0; i < n; i++) {
+            Water += (Math.min(l[i], r[i])) - height[i];
+        }
+        return Water;
+    }
+}
