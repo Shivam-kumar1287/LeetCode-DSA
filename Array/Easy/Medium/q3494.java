2657ee3452b98436fe7b03488ca7c89ad86f06f1
Shivam Kumar <kshivam0508@gmail.com>
2025-10-09 23:22:39 +0530

    Array/Easy/Easy/q169.java

---
+++ Array/Easy/Medium/q3494.java
@@ -0,0 +1,25 @@
+class q3494 {
+    public long minTime(int[] skill, int[] mana) {
+        int n=skill.length;
+        long[] prefix=new long[n];
+        prefix[0]=skill[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+skill[i];
+        }
+       long starttime=0;
+       int m=mana.length;
+        for(int i=1;i<mana.length;i++){
+           long maxdiff=Long.MIN_VALUE;
+            for(int j=0;j<n;j++){
+                long left = (j > 0) ? prefix[j - 1] : 0;
+                long cur=(mana[i-1]*prefix[j])-(mana[i]*left);
+                if(cur>maxdiff){
+                    maxdiff=cur;
+                }
+            }
+            starttime+=maxdiff;
+        }
+        return starttime +mana[m-1]*prefix[n-1];
+        
+    }
+}
