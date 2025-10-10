4a98cb24931d4a62528670765db7523037ccb0e0
Shivam Kumar <kshivam0508@gmail.com>
2025-10-10 21:29:38 +0530

    Tree/medium/q199.java

--- Tree/medium/q199.java
+++ Tree/medium/q199.java
@@ -16,13 +16,13 @@
 class q199 {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> list=new ArrayList<>();
-        return helper(list,root,0);
+           return helper(list,root,0);
     }
     public List<Integer> helper(List<Integer> list, TreeNode root,int l){
         if(root==null)return list;
         if(l==list.size()){
             list.add(root.val);}
-        helper(list,root.right,l+1);
+         helper(list,root.right,l+1);
         helper(list,root.left,l+1);
         return list;
     }
