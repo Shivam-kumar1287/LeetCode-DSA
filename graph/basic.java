00f1c5871aed472a9ff0da8e5f1877ffc94d881d
Shivam Kumar <kshivam0508@gmail.com>
2025-10-17 23:37:02 +0530

    graph/basic.java dfs

--- graph/basic.java
+++ graph/basic.java
@@ -1,12 +1,28 @@
 package leetcode.graph;
+
 import java.util.ArrayList;
 import java.util.Queue;
 
-public class basic {
+/*
+ * Basic Graph representation using Adjacency List
+ * and implementations of DFS and BFS traversals.
+ * Vertices are numbered from 0 to V-1.
+ * Undirected and weighted graph.
+ *          1
+ *     0 ----- 2
+ *   | \     |
+ * |  \2   |3
+ * 5 ----- 4
+ *   
+ */
+
+public class Basic {
+
     static class Edge {
         int src;
         int dest;
         int wt;
+
         Edge(int s, int d, int w) {
             this.src = s;
             this.dest = d;
@@ -21,14 +37,15 @@ public class basic {
             graph[i] = new ArrayList<>();
         }
 
-        // undirected edges
+        // Add undirected edges
         addEdge(graph, 0, 1, 1);
         addEdge(graph, 0, 2, 1);
         addEdge(graph, 1, 3, 2);
         addEdge(graph, 2, 3, 2);
         addEdge(graph, 1, 4, 3);
 
-        // print graph
+        // Print graph
+        System.out.println("Graph representation:");
         for (int i = 0; i < V; i++) {
             System.out.print("Vertex " + i + " -> ");
             for (Edge e : graph[i]) {
@@ -36,30 +53,54 @@ public class basic {
             }
             System.out.println();
         }
-        System.out.println("BFS Traversal:");
+
+        // BFS Traversal
+        System.out.println("\nBFS Traversal:");
         bfs(graph);
+
+        // DFS Traversal
+        System.out.println("\nDFS Traversal:");
+        boolean[] vis = new boolean[V];
+        dfs(graph, 0, vis);
+        System.out.println();
     }
 
+    // Method to add undirected edge
     static void addEdge(ArrayList<Edge>[] graph, int src, int dest, int wt) {
         graph[src].add(new Edge(src, dest, wt));
         graph[dest].add(new Edge(dest, src, wt)); // reverse edge for undirected graph
     }
-    public static void bfs(ArrayList<Edge>[] graph ) {
+
+    // DFS traversal
+    public static void dfs(ArrayList<Edge>[] graph, int curr, boolean[] vis) {
+        System.out.print(curr + " ");
+        vis[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                dfs(graph, e.dest, vis);
+            }
+        }
+    }
+
+    // BFS traversal
+    public static void bfs(ArrayList<Edge>[] graph) {
         Queue<Integer> q = new java.util.LinkedList<>();
         boolean[] visited = new boolean[graph.length];
-        q.add(0);
-        while(!q.isEmpty()){
-            int curr=q.remove();
-            if(!visited[curr]){
-                System.out.println(curr+" ");
-                visited[curr]=true;
-                for(int i=0;i<graph[curr].size();i++){
-                    Edge e=graph[curr].get(i);
-                    q.add(e.dest);
 
+        q.add(0);
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            if (!visited[curr]) {
+                System.out.print(curr + " ");
+                visited[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
                 }
             }
         }
-        
+        System.out.println();
     }
+    
 }
