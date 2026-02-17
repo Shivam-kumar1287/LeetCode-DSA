804b745695d63f5096a33e48a6d3a6b1f87321be
Shivam Kumar <kshivam0508@gmail.com>
2026-02-17 17:20:47 +0530

    new

--- DP/kn/knapsack.java
+++ DP/kn/knapsack.java
@@ -13,7 +13,7 @@ public static int knapSack1(int w,int wt[],int val[],int n){
     if(wt[n-1]<=w){
         int ans=val[n-1]+knapSack1(w, wt, w-wt[n-1], n);
         int ans2=knapSack1(w, wt, val, n-1);
-        return Math.max(ans, ans2);
+          return Math.max(ans, ans2);
 
     }
     else{
