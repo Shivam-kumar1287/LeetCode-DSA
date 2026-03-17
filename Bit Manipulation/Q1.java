6f257cf7c40c11146379201a258177bc027241b9
Shivam Kumar <kshivam0508@gmail.com>
2026-03-17 23:39:40 +0530

    add about  bit

--- Bit Manipulation/Q1.java
+++ Bit Manipulation/Q1.java
@@ -5,7 +5,7 @@ class Q1{
         System.out.println("Before swapping: a = " + a + ", b = " + b);
         a=a^b; // a now holds the result of a XOR b
         b=a^b; // b now holds the original value of a
-        a=a^b; // a now holds the original value of b
+     a=a^b; // a now holds the original value of b
         System.out.println("After swapping: a = " + a + ", b = " + b);
     }
 }
