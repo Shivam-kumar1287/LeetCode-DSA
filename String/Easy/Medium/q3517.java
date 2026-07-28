edbd0b767d61f249599267906fc8d635daa692a2
Shivam Kumar <kshivam0508@gmail.com>
2026-07-28 20:56:04 +0530

    add q3517:>)

---
+++ String/Easy/Medium/q3517.java
@@ -0,0 +1,33 @@
+class q3517 {
+    public String smallestPalindrome(String s) {
+
+        char[] ch = s.toCharArray();
+        Arrays.sort(ch);
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb1 = new StringBuilder();
+
+        String t = "";
+
+        int i = 1;
+        while (i < ch.length) {
+            if (ch[i] == ch[i - 1]) {
+                sb.append(ch[i]);   // one character for left half
+                i++;                // skip the paired character
+            } else {
+                t = String.valueOf(ch[i - 1]);
+            }
+            i++;
+        }
+
+        // Last character may be the middle one
+        if (i == ch.length && ch.length % 2 == 1) {
+            t = String.valueOf(ch[ch.length - 1]);
+        }
+
+        sb1.append(sb);
+        sb1.reverse();
+
+        return sb.toString() + t + sb1.toString();
+    }
+}
